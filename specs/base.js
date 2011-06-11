0800de1d9f81e770f9b4d28d1071bd70de163fe0
GarrenSmith <garren.smith@gmail.com>
2011-06-11 15:07:22 +0200

    basic modelling

--- specs/base.js
+++ specs/base.js
@@ -1,15 +1,20 @@
 var describe = require('Jody').describe,
-    Model = require('../lib/index');
+    Model = require('../lib/index'),
+    assert = require('assert');
 
-var user = Model('User', {
-  name: String,
-  surname: String
-});
 
 
 
 describe('Create Model').
+  beforeEach(function (done) {
+    Model.remove_models();
+  }).
   it("Should create basic model given json", function () {
+    var user = Model('User', {
+        name: String,
+        surname: String,
+    });
+
     var raw_user = {
       name: "Garren",
       surname: "Smith"
@@ -19,5 +24,46 @@ describe('Create Model').
     
     created_user.name.should().beEqual('Garren');
     created_user.surname.should().beEqual('Smith');
+
+  }).
+  it("Should Supply defaults for unsupplied values", function () {
+    var user = Model('User', {
+      name: String,
+      surname: String,
+      age: {type: String, default:"3"}
+    });
+
+    var raw_user = {
+      name: "Garren",
+      surname: "Smith"
+    };
+
+    var created_user = Model.load('User',raw_user);
+
+    console.dir(created_user);
+    Model.dump();
+ 
+    created_user.name.should().beEqual('Garren');
+    created_user.surname.should().beEqual('Smith');
+    created_user.age.should().beEqual('3');
+
+  }).
+  it("Should undefined propety if no default defined", function () {
+    var user = Model('User', {
+      name: String,
+      surname: String,
+      age: String
+    });
+
+    var raw_user = {
+      name: "Garren",
+      surname: "Smith"
+    };
+
+    var created_user = Model.load('User',raw_user);
+ 
+    created_user.name.should().beEqual('Garren');
+    created_user.surname.should().beEqual('Smith');
+    assert.equal(created_user.age, undefined);
 
   });
