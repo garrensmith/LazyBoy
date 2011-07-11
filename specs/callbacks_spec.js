be1fe5dd4bfb48eea9c2f2d5def60f3971c29ebe
GarrenSmith <garren.smith@gmail.com>
2011-07-11 22:53:48 +0200

    basic callbacks on save but it aint pretty

--- specs/callbacks_spec.js
+++ specs/callbacks_spec.js
@@ -1,23 +1,42 @@
 var describe = require('Jody').describe,
+    assert =require('assert'),
     Model = require('../lib/index');
 
-describe('BeforeSave callback').
-  it("Should be called", function (async) {
-    var Blog = Model.define('Blog',{title: String,
+describe('Save callback').
+  beforeAll(function (done) {
+    Model.define('Blog',{title: String,
                                     post: String,
                                     url: String
                                     });
 
-    Blog.beforeSave(function (blog, done) {
-      blog.url = title.split(' ').join('-');
-      done();
+    done();
+  }).
+  it("should called beforeSave before Save", function (async) {
+    var Blog = Model('Blog');
+
+
+    Blog.beforeSave(function (blog ) {
+      console.dir(blog);
+      blog.url = blog.title.split(' ').join('-');
     });
 
     var blog_post = Blog.create({title: "hello world", post:"My first demo post"});
     
     blog_post.save(async(function (err, saved_blog_post) {
-      saved_blog_post.url.should().beEqual("hello-word");
+     console.dir(saved_blog_post);
+     saved_blog_post.url.should().beEqual("hello-world");
     }));
 
+  }).
+  it("Should call afterSave after save", function (async) {
+    var Blog = Model('Blog');
+
+    var blog_post = Blog.create({title: "hello again", post:"My 2nd demo post"});
+    blog_post.save();
+
+    Blog.afterSave(async(function (blog) {
+        console.log("after save");
+        assert.notEqual(blog.id,null);
+    }));
   });
 
