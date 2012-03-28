c52c677352efe39a95ebab3fafafca262f3654af
Garren Smith <garren.smith@gmail.com>
2012-03-28 21:59:15 +0200

    remove all docs from db

--- specs/spec_helper.js
+++ specs/spec_helper.js
@@ -1,31 +1,21 @@
 var db_connection = require('../lib/connection.js'),
     cradle = require('cradle');
+
 //var db = module.exports.db = new (cradle.Connection)('https://garrensmith.cloudant.com','443',{secure: true}).database('lazyboy_ci_test');
 var db = module.exports.db = new (cradle.Connection)().database('lazyboy_tests');
-require('Jody').configure.beforeAll(function (done) {
 
-  //require('../lib/logger').setLogLevel(7);
 
-  db.destroy(function (err) {  
-    if (err) {
-      console.log("error on delete db");
-      //console.dir(err);
-    }
+before(function(done) {
+    db.get('_all_docs', function (err, res) {
+        
+        JSON.parse(res).forEach(function (item) {
+            db.remove(item.id, item.value.rev, function (err, res) {});
+        });
 
-    db.create(function () {
-      db_connection.create_connection('lazyboy_tests');
-      /*db_connection.create_connection({
-        url: 'https://garrensmith.cloudant.com',
-        port: '443',
-        db:'lazyboy_ci_test',
-        secure:true,
-        });*/
-      done();   
+        done();
     });
-  });
+
 });
-
-
 
 
 
